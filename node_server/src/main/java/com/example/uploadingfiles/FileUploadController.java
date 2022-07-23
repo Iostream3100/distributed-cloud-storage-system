a2a12c6f986c65af49a86a0d7d5622dc39211fd5
Yun Feng <iostream3100@gmail.com>
2022-07-22 23:16:06 -0700

    add an api to get files in a flolder by its path

--- node_server/src/main/java/com/example/uploadingfiles/FileUploadController.java
+++ node_server/src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -1,11 +1,16 @@
 package com.example.uploadingfiles;
 
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,47 +31,71 @@ import com.example.uploadingfiles.storage.StorageService;
 @Controller
 public class FileUploadController {
 
-	private final StorageService storageService;
+    private final StorageService storageService;
 
-	@Autowired
-	public FileUploadController(StorageService storageService) {
-		this.storageService = storageService;
-	}
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
-	@GetMapping("/")
-	public String listUploadedFiles(Model model) throws IOException {
+    /**
+     * get all the files and directories under of a directory
+     *
+     * @param path path of the directory
+     * @return A List of String with the name of paths and directories
+     */
+    @GetMapping("/dirs")
+    @ResponseBody
+    public List<String> getDirectoriesByPath(@RequestParam(value = "path", defaultValue = "") String path) {
+        try {
+            Stream<Path> pathStream = storageService.loadAllByPath(path);
+            return pathStream.map(Path::toString).collect(Collectors.toList());
+        } catch (NoSuchFileException exc) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Invalid path, directory not found", exc);
+        }
+    }
 
-		model.addAttribute("files", storageService.loadAll().map(
-				path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-						"serveFile", path.getFileName().toString()).build().toUri().toString())
-				.collect(Collectors.toList()));
+    @GetMapping("/")
+    public String listUploadedFiles(Model model) throws IOException {
 
-		return "uploadForm";
-	}
+        Stream<Path> pathStream = storageService.loadAll();
+        pathStream.forEach(path -> {
+            System.out.println(path.toString());
 
-	@GetMapping("/files/{filename:.+}")
-	@ResponseBody
-	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        });
+        model.addAttribute("files", storageService.loadAll().map(
+                        path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                                "serveFile", path.getFileName().toString()).build().toUri().toString())
+                .collect(Collectors.toList()));
 
-		Resource file = storageService.loadAsResource(filename);
-		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
-	}
+        return "uploadForm";
+    }
 
-	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file,
-			RedirectAttributes redirectAttributes) {
 
-		storageService.store(file);
-		redirectAttributes.addFlashAttribute("message",
-				"You successfully uploaded " + file.getOriginalFilename() + "!");
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
-		return "redirect:/";
-	}
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
 
-	@ExceptionHandler(StorageFileNotFoundException.class)
-	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-		return ResponseEntity.notFound().build();
-	}
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
 
 }
--- node_server/src/main/java/com/example/uploadingfiles/storage/StorageService.java
+++ node_server/src/main/java/com/example/uploadingfiles/storage/StorageService.java
@@ -3,6 +3,7 @@ package com.example.uploadingfiles.storage;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
@@ -13,6 +14,8 @@ public interface StorageService {
 	void store(MultipartFile file);
 
 	Stream<Path> loadAll();
+
+	Stream<Path> loadAllByPath(String rootPath) throws NoSuchFileException;
 
 	Path load(String filename);
 
