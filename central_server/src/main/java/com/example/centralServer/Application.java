af3a972f3cce0c4febcc6a67ad8440ccd1cbaeb3
panmq122 <pan.mei@northeastern.edu>
2022-08-16 18:48:19 -0700

    latest push

--- central_server/src/main/java/com/example/centralServer/Application.java
+++ central_server/src/main/java/com/example/centralServer/Application.java
@@ -1,15 +1,15 @@
-package com.example.centralServer;
-
-
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-}
+//package com.example.centralServer;
+//
+//
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class Application {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//
+//}
